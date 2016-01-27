7919be4b62a1bf11aabc943d37f3111ee7ce9084
ryh <1971990184@qq.com>
2016-01-27 17:35:22 +0800

    third commit

--- novelspider/spiders/novspider.py
+++ novelspider/spiders/novspider.py
@@ -5,12 +5,14 @@ from scrapy.selector import Selector
 from scrapy.http import Request
 from novelspider.items import NovelspiderItem
 import re
-
+STOCKCODE = '000666'
 class novSpider(RedisSpider):
     name = "novspider"
     redis_key = 'nvospider:start_urls'
-    stockCode = '000555'
-    start_urls = ['http://guba.eastmoney.com/list,' + stockCode + ',5_' + str(2) + '.html'
+    global STOCKCODE
+    # for content in STOCKCODE:
+    #     start_urls =
+    start_urls = ['http://guba.eastmoney.com/list,' + STOCKCODE + ',5_' + '1.html'
                   ]
 
     def parse(self,response):
@@ -20,7 +22,12 @@ class novSpider(RedisSpider):
             read = each.xpath('span[1]/text()').extract()[0]
             comment = each.xpath('span[2]/text()').extract()[0]
             title = each.xpath('span[3]/a/text()').extract()[0]
-            author = each.xpath('span[4]/a/text()').extract()[0]
+            # author容易因为结构出现异常
+            if each.xpath('span[4]/a/text()'):
+                author = each.xpath('span[4]/a/text()').extract()[0]
+            else:
+                author = each.xpath('span[4]/span/text()').extract()[0]
+
             date = each.xpath('span[5]/text()').extract()[0]
             last = each.xpath('span[6]/text()').extract()[0]
             address = each.xpath('span[3]/a/@href').extract()[0]
@@ -38,17 +45,19 @@ class novSpider(RedisSpider):
         info = selector.xpath('//*[@id="articlelistnew"]/div[@class="pager"]/span/@data-pager').extract()[0]
         List = info.split('|')
         # 定义需要抓取多少页
-        if int(List[3])<8:
+        if int(List[2])*int(List[3])<int(List[1]):
             # 生成页面链接
-            nextLink = 'http://guba.eastmoney.com/list,' + '000555' + ',5_' + str(int(List[3])+1) + '.html'
-        # 抓取页面并且处理
-        yield Request(nextLink,callback=self.parse)
+            nextLink = 'http://guba.eastmoney.com/list,' + STOCKCODE + ',5_' + str(int(List[3])+1) + '.html'
+            # 抓取页面并且处理
+            yield Request(nextLink,callback=self.parse)
+
+
 
 
     def parseContent(self, response):
         selector = Selector(response)
         item = response.meta['item']
         data = selector.xpath('//div[@class="stockcodec"]')
-        info = data.xpath('string(.)').extract()[0]
+        info = data.xpath('string(.)').extract()[0].replace('\n','').replace('\r','').replace('\t','')
         item['text'] = info
         yield item
